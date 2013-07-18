b751bdae310df8cbb6942b816c60f463acb56960
Kaushal Kumar <kaushal499@gmail.com>
2013-07-18 17:47:10 +0000

    restored web.js

--- web.js
+++ web.js
@@ -1,8 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var express = require('express');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(index);
-}).listen(9615);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World 2!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
